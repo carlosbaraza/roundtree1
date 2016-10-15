01fb61ffd6efca7744b5719ca65933f86d9b0798
Carlos Baraza Haro <carlos.baraza@ieee.org>
2016-10-15 18:52:20 +0100

    Add new gallery page

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -5,12 +5,13 @@ import AppContainer from '../../ui/containers/AppContainer.jsx';
 // import AuthPageSignIn from '../../ui/pages/AuthPageSignIn.js';
 // import AuthPageJoin from '../../ui/pages/AuthPageJoin.js';
 // import NotFoundPage from '../../ui/pages/NotFoundPage.jsx';
+import NewGalleryContainer from '../../ui/containers/galleries/NewGalleryContainer.jsx';
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={AppContainer}>
-      {/*<IndexRoute component={NotFoundPage}/>
-      <Route path="*" component={NotFoundPage}/>*/}
+      <IndexRoute component={NewGalleryContainer}/>
+      <Route path="*" component={NewGalleryContainer}/>
       {/*<Route path="signin" component={AuthPageSignIn}/>
       <Route path="join" component={AuthPageJoin}/>*/}
     </Route>
---
+++ imports/ui/pages/galleries/NewGallery.jsx
@@ -0,0 +1,77 @@
+import React, { Component, PropTypes } from 'react';
+import CircularProgress from 'material-ui/CircularProgress';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import CloudUpload from 'material-ui/svg-icons/file/cloud-upload';
+import _ from 'lodash';
+
+const styles = {
+  button: {
+  },
+  imageInput: {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    width: '100%',
+    opacity: 0,
+    fontSize: 0
+  },
+};
+
+export default class NewGallery extends Component {
+  render() {
+    const { loading } = this.props;
+
+    return (
+      <div className="new-gallery">
+        {(() => {
+          if (loading) {
+            return (
+              <CircularProgress
+                size={2}
+                style={{
+                  marginLeft: 'auto',
+                  marginRight: 'auto',
+                  display: 'block',
+                }}
+              />
+            );
+          } else {
+            return (
+              <div>
+                <div>
+                  <TextField
+                    floatingLabelText="Your gallery name"
+                    fullWidth={true}
+                  />
+                </div>
+
+                <RaisedButton
+                  label="images"
+                  labelPosition="before"
+                  primary={true}
+                  icon={<CloudUpload />}
+                  style={styles.button}
+                  fullWidth={true}
+                >
+                  <input
+                    className="file-chooser"
+                    type="file"
+                    style={styles.imageInput}
+                    multiple={true}
+                  />
+                </RaisedButton>
+              </div>
+            );
+          }
+        })()}
+      </div>
+    );
+  }
+}
+
+NewGallery.propTypes = {
+};
