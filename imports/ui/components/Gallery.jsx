bef6eb045a04cb0dfb369c4cdbd5530bb793aba5
Carlos Baraza Haro <carlos.baraza@ieee.org>
2016-10-16 23:14:14 +0100

    Display the normal image as thumbnail (temporary)

--- imports/ui/components/Gallery.jsx
+++ imports/ui/components/Gallery.jsx
@@ -4,10 +4,20 @@ import JustifiedGallery from 'react-grid-gallery';
 
 export default class Gallery extends Component {
   render() {
+    const images = this.props.images.map(image => {
+      return {
+        src: image.src,
+        thumbnail: image.src,
+        thumbnailWidth: image.thumbnailWidth,
+        thumbnailHeight: image.thumbnailHeight,
+        caption: image.caption
+      };
+    });
+
     return (
       <div className="gallery">
         <JustifiedGallery
-          images={this.props.images}
+          images={images}
           enableImageSelection={false}
           rowHeight={150}
           margin={1}
