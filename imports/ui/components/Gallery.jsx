8eed5ed8dc0846381e5fa8f550f161359afe2d32
Carlos Baraza Haro <carlos.baraza@ieee.org>
2016-10-16 23:09:57 +0100

    Fix indentation and console errors

--- imports/ui/components/Gallery.jsx
+++ imports/ui/components/Gallery.jsx
@@ -3,18 +3,18 @@ import React, { Component, PropTypes } from 'react';
 import JustifiedGallery from 'react-grid-gallery';
 
 export default class Gallery extends Component {
-    render() {
-        return (
-            <div className="gallery">
-                <JustifiedGallery
-                  images={this.props.images}
-                  enableImageSelection={false}
-                  rowHeight={150}
-                  margin={1}
-                  showCloseButton={false}
-                  backdropClosesModal={true}
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="gallery">
+        <JustifiedGallery
+          images={this.props.images}
+          enableImageSelection={false}
+          rowHeight={150}
+          margin={1}
+          showCloseButton={false}
+          backdropClosesModal={true}
+        />
+      </div>
+    );
+  }
 }
--- imports/ui/pages/ShowGallery.jsx
+++ imports/ui/pages/ShowGallery.jsx
@@ -1,40 +1,37 @@
 import React, { Component } from 'react';
 import Gallery from '/imports/ui/components/Gallery.jsx';
 
-import RefreshIndicator from 'material-ui/RefreshIndicator';
+import CircularProgress from 'material-ui/CircularProgress';
 
 export default class ShowGallery extends Component {
-    render() {
-        const style = {
-            container: {
-                position: 'relative'
-            },
-            refresh: {
-                display: 'inline-block',
-                position: 'relative'
-            }
-        };
+  render() {
+    const style = {
+      container: {
+        position: 'relative'
+      },
+      spinner: {
+        display: 'inline-block',
+        position: 'relative'
+      }
+    };
 
-        if (this.props.imagesReady) {
-            return (
-                <div>
-                    <Gallery images={this.props.images} />
-                </div>
-            );
-        }
-        return (
-            <div className="show-gallery-container" style={style.container}>
-                <div className="show-gallery">
-                    <RefreshIndicator
-                        size={100}
-                        left={0}
-                        top={0}
-                        loadingColor="#FF9800"
-                        status="loading"
-                        style={style.refresh}
-                    />
-                </div>
-            </div>
-        );
+    if (this.props.imagesReady) {
+      return (
+        <div>
+          <Gallery images={this.props.images} />
+        </div>
+      );
     }
+    return (
+      <div className="show-gallery-container" style={style.container}>
+        <div className="show-gallery">
+          <CircularProgress
+            size={100}
+            thickness={8}
+            style={style.spinner}
+          />
+        </div>
+      </div>
+    );
+  }
 }
